c465412392b0a41e9825406eae75a3120cdd85a1
Tyler Norlund <tnorlund@icloud.com>
2020-12-18 12:21:04 -0800

    ✍️ Added a post about Xcode

--- src/components/MdxImage/index.js
+++ src/components/MdxImage/index.js
@@ -1,12 +1,11 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import { Icon } from 'components/styles'
 
 export default function MdxIcon( { filename } ) {
   let url = ``
   const result = useStaticQuery(
     graphql`query {
-      allFile( filter: { absolutePath: { regex: "/.svg$/" } }) {
+      allFile( filter: { absolutePath: { regex: "/.png$/" } }) {
         nodes{ publicURL }
       }
     }`
@@ -14,6 +13,6 @@ export default function MdxIcon( { filename } ) {
   result.allFile.nodes.forEach( node => {
     if ( node.publicURL.endsWith( filename ) ) url = node.publicURL
   } )
-  if ( !url ) throw new Error( `Cannot find '.svg' ${filename}` )
-  return <Icon src={url} width={`100%`}/>
+  if ( !url ) throw new Error( `Cannot find '.png' ${filename}` )
+  return <img src={url} width={`100%`}/>
 }
