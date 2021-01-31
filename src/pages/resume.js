1f2d3eaf4c8eb36cc033656ee749f0f5a7701e2f
Tyler Norlund <tnorlund@icloud.com>
2021-01-30 16:22:29 -0800

    🔨 Added Pandas

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -13,6 +13,8 @@ import Docker from 'components/Icons/Docker'
 import Aws from 'components/Icons/Aws'
 import Openmpi from 'components/Icons/Openmpi'
 import Tensorflow from 'components/Icons/Tensorflow'
+import React_icon from 'components/Icons/React'
+import Pandas from 'components/Icons/Pandas'
 
 /**
  *
@@ -24,21 +26,6 @@ function Duties( { duties } ) {
   )
   return <ul>{dutiesList}</ul>
 }
-
-const SkillDiv = styled.div`
-border: solid 2px var(--color-b);
-border-radius: 0.3em;
-padding-right: 0.6em;
-padding-left: 0.6em;
-font-weight: bold;
-margin: 0.6em;
-`
-const SkillsDiv = styled.div`
-display: flex;
-flex-direction: row;
-flex-wrap: wrap;
-justify-content: space-evenly;
-`
 
 const JobDiv = styled.div`
 border-bottom: 2px solid var(--color-b);
@@ -63,17 +50,6 @@ const MainTitle = styled.h1`
 border-bottom: 4px solid var(--color-b);
 `
 
-const Description = styled.div`
-margin-bottom: 0.3em;
-`
-
-function Skills( { skills } ) {
-  const skillList = skills.map( ( skill ) =>
-    <SkillDiv key={skill}>{skill}</SkillDiv>
-  )
-  return <SkillsDiv>{skillList}</SkillsDiv>
-}
-
 export default function Resume() {
   const { contentYaml } = useStaticQuery( graphql`
     {
@@ -81,12 +57,11 @@ export default function Resume() {
         positions {
           title, company, start(formatString: "MMM YYYY"),
           end(formatString: "MMM YYYY"), duties
-        },
-        skills
+        }
       }
     }
   ` )
-  const { positions, skills } = contentYaml
+  const { positions } = contentYaml
   const user = useSessionStorage( AUTH_KEY )[0]
   useEffect( () => {
     FireHose( `Resume`, `/resume`, user )
@@ -94,12 +69,7 @@ export default function Resume() {
   return(
     <PageBody>
       <Toc />
-      <MainTitle>What I&apos;ve Done</MainTitle>
-      <Description>
-        I&apos;ve worked with both hardware and software at a few
-        companies. I&apos;m currently trying to find a position related to
-        computer vision and/or data science.
-      </Description>
+      <MainTitle>Experience</MainTitle>
       {positions.map( ( { title, company, start, end, duties } ) =>
         <div key={ title + company }>
           <JobDiv>
@@ -110,33 +80,26 @@ export default function Resume() {
           <Duties duties={duties} />
         </div>
       ) }
-      <MainTitle>Education</MainTitle>
-      <Description>
-        I did both my undergraduate and graduate school with University of
-        the Pacific. This school was great, and I had the opportunity to
-        learn a lot and develop life-long relationships.
-      </Description>
+      <MainTitle>Education </MainTitle>
       <JobDiv>
-        <JobTitle>Data Science</JobTitle>
-        <JobLocation>Master&apos;s Degree</JobLocation>
+        <JobTitle>MS: Data Science</JobTitle>
+        <JobLocation>University of the Pacific</JobLocation>
         <JobDate>Aug 2018 - May 2020</JobDate>
       </JobDiv>
       <JobDiv>
-        <JobTitle>Computer Engineering</JobTitle>
-        <JobLocation>Bachelor&apos;s Degree</JobLocation>
+        <JobTitle>BS: Computer Engineering</JobTitle>
+        <JobLocation>University of the Pacific</JobLocation>
         <JobDate>Aug 2013 - May 2018</JobDate>
       </JobDiv>
-      <MainTitle>My Skills</MainTitle>
-      <Description>
-        Through school or work, I&apos;ve always tried to learn as many skills
-        as possible. Here are the skills I&apos;ve picked up thus far.
-      </Description>
+      <MainTitle>Skills</MainTitle>
       <Aws />
-      <Terraform />
+      <Docker />
       <Opencv />
       <Openmpi />
-      <Docker />
+      <Pandas />
+      <React_icon />
       <Tensorflow />
+      <Terraform />
     </PageBody>
   )
 }
