99f1f88212334da8c2557b2fbc84378343dcb813
Tyler Norlund <tnorlund@icloud.com>
2021-02-02 21:32:29 -0800

    🐛 Squashed bug of bad analytics modal view

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,10 @@ import Authentication from '../Authentication'
 import Privacy from '../Privacy'
 import { HeaderDiv, Logo, IconDiv, Div, Icon } from './styles'
 import { Configure } from 'utils/auth'
-import { useSessionStorage } from 'hooks'
-import { PRIVACY_KEY } from 'utils/constants'
+import { useSessionStorage, useEventListener } from 'hooks'
+import { PRIVACY_KEY, VISITOR_KEY } from 'utils/constants'
+import { v4 as uuidv4 } from 'uuid'
+
 
 /** Ensure that Amplify is configured on each page. */
 Configure()
@@ -16,11 +18,19 @@ export default function Header( { site } ) {
   const [open, setModal] = useState( false )
   // Logic for showing whether privacy
   const privacy = useSessionStorage( PRIVACY_KEY )[0]
+  const [ visitorKey, setVisitorKey ] = useSessionStorage( VISITOR_KEY )
+  if ( !visitorKey ) setVisitorKey( uuidv4() )
   // Show the privacy view when is is the visitor's first time.
-  const [privacyShow, setPrivacyShow] = useState(
-    !privacy ||
-    ( privacy && privacy.shownWindow == false )
-  )
+  const [privacyShow, setPrivacyShow] = useState( false )
+  useEventListener( `scroll`, () => {
+    if (
+      window.scrollY > 100 &&
+      (
+        !privacy ||
+        ( privacy && privacy.shownWindow == false )
+      )
+    ) setPrivacyShow( true )
+  } )
   return (
     <>
       <HeaderDiv>
